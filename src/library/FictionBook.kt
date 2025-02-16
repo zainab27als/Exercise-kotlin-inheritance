bff4dab64e25f89c3bb99569b97471b45aa0899b
Zainab 27 <zainabalsaffar03@gmail.com>
2025-02-16 20:31:16 +0300

    Step 3 done

--- src/library/FictionBook.kt
+++ src/library/FictionBook.kt
@@ -1,2 +1,14 @@
 package library
 
+class FictionBook(
+    title: String,
+    author: String,
+    isbn: String,
+    val genre: String
+) : Book(title, author, isbn) {
+
+    override fun displayInfo() {
+        super.displayInfo()
+        println("Genre: $genre")
+    }
+}
